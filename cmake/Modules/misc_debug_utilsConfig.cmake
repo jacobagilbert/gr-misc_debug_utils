b01ea486763c0a5efa6288556df36afb41d7c8e2
Jacob Gilbert <mrjacobagilbert@gmail.com>
2016-10-14 11:01:43 -0600

    added module and first block

---
+++ cmake/Modules/misc_debug_utilsConfig.cmake
@@ -0,0 +1,30 @@
+INCLUDE(FindPkgConfig)
+PKG_CHECK_MODULES(PC_MISC_DEBUG_UTILS misc_debug_utils)
+
+FIND_PATH(
+    MISC_DEBUG_UTILS_INCLUDE_DIRS
+    NAMES misc_debug_utils/api.h
+    HINTS $ENV{MISC_DEBUG_UTILS_DIR}/include
+        ${PC_MISC_DEBUG_UTILS_INCLUDEDIR}
+    PATHS ${CMAKE_INSTALL_PREFIX}/include
+          /usr/local/include
+          /usr/include
+)
+
+FIND_LIBRARY(
+    MISC_DEBUG_UTILS_LIBRARIES
+    NAMES gnuradio-misc_debug_utils
+    HINTS $ENV{MISC_DEBUG_UTILS_DIR}/lib
+        ${PC_MISC_DEBUG_UTILS_LIBDIR}
+    PATHS ${CMAKE_INSTALL_PREFIX}/lib
+          ${CMAKE_INSTALL_PREFIX}/lib64
+          /usr/local/lib
+          /usr/local/lib64
+          /usr/lib
+          /usr/lib64
+)
+
+INCLUDE(FindPackageHandleStandardArgs)
+FIND_PACKAGE_HANDLE_STANDARD_ARGS(MISC_DEBUG_UTILS DEFAULT_MSG MISC_DEBUG_UTILS_LIBRARIES MISC_DEBUG_UTILS_INCLUDE_DIRS)
+MARK_AS_ADVANCED(MISC_DEBUG_UTILS_LIBRARIES MISC_DEBUG_UTILS_INCLUDE_DIRS)
+
---
+++ python/dropped_samp_detector.py
@@ -0,0 +1,81 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# 
+# Copyright 2016 <+YOU OR YOUR COMPANY+>.
+# 
+# This is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3, or (at your option)
+# any later version.
+# 
+# This software is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this software; see the file COPYING.  If not, write to
+# the Free Software Foundation, Inc., 51 Franklin Street,
+# Boston, MA 02110-1301, USA.
+# 
+
+import numpy
+import pmt
+from gnuradio import gr
+
+class dropped_samp_detector(gr.sync_block):
+    """
+    docstring for block dropped_samp_detector
+    """
+    def __init__(self):
+        gr.sync_block.__init__(self,
+            name="dropped_samp_detector",
+            in_sig=[numpy.complex64],
+            out_sig=None)
+
+        self.rate = 1000000     # this will get overwritten by samp 0 tag
+        self.base_time = 0      # this will get overwritten by samp 0 tag
+
+
+    def work(self, input_items, output_items):
+        nitems = input_items[0].size
+        start = self.nitems_read(0)
+        end = start + nitems
+
+        # get tags
+        tags = self.get_tags_in_range(0, start, end)
+
+        # first go through and check for rx_time tags
+        for tag in tags:
+            if pmt.equal(tag.key, pmt.intern('rx_time')):
+                # on the first sample, don't check for errors
+                if tag.offset == 0:
+                    self.base_time = sum(pmt.to_python(tag.value))
+                    print "first tag " + repr(tag.value) + ", base_time set to " + repr(self.base_time)
+
+                # other times we get this, check for errors
+                else:
+                    time = sum(pmt.to_python(tag.value))
+                    samp = (time - self.base_time) * self.rate
+                    error = round(samp - tag.offset, 5)
+
+
+                    if error:
+                        duration_us = 1000000.0 * error / self.rate
+                        print "\nOVERFLOW ERROR !  " + repr(error) + " S, " + repr(round(duration_us,2)) + " us ",
+                        #print "\n\tTAG VAL " + repr(tag.value) + " received at " + repr(tag.offset) + " - sample " + repr(round(samp, 5)) + " ",
+
+                        # update zero sample time so error does not accumulate
+                        self.base_time = self.base_time + error / self.rate
+
+
+        # if the rate was changed, save it...this shouldn't happen often
+        for tag in tags:
+            if pmt.equal(tag.key, pmt.intern('rx_rate')):
+                oldrate = self.rate
+                self.rate = pmt.to_python(tag.value)
+                if (self.rate != oldrate) and tag.offset:
+                    print "\nWARNING! rate change - new rate is " + repr(self.rate) + " " + repr(oldrate),
+
+        # consume/produce everything
+        return nitems
